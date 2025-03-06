0270d9c80186b515edbd7675ff4ca849d7f833b3
M.A.S <m.alex.vi.silva@gmail.com>
2025-03-06 13:27:29 -0400

    Delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿using Delegates.classes;
+
+
+// Delegate that uses Calculadora.Somar()
+public delegate int Operacao(int a, int b);
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Operacao op = Calculadora.Somar;
+        int resultado = op(10, 20);
+        Console.WriteLine($"Soma: {resultado}");
+        
+        op = Calculadora.Subtrair;
+        resultado = op(10, 20);
+        Console.WriteLine($"Subtração: {resultado}");
+    }
+}
